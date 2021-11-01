f78286b8d231a6bea9333242ed28e17578f1c335
{菊地泰斗} <{taitoo0402@gmail.com}>
2021-11-02 00:32:52 +0900

    出力を見やすくした

--- kadai3/kadai1-3.c
+++ kadai3/kadai1-3.c
@@ -63,11 +63,11 @@ double df(double x, double n){
 }
 
 int main(){
-    double x_st1 = 0.85;
-    double x_st2 = 0.5;
+    double x_st5 = 0.85;
+    double x_st4 = 0.5;
     double x_st3 = 0.1;
-    double x_st4 = -0.60;
-    double x_st5 = -0.85;
+    double x_st2 = -0.60;
+    double x_st1 = -0.85;
     double esp = 100;
 
     double x;
@@ -77,36 +77,35 @@ int main(){
         x_st1 = x_st1 - f(x_st1,5)/df(x_st1,5);
         esp = fabs(x_st1 - x);
     }
-    printf("%.9lf\n", x_st1);
+    printf("solution1:%.9lf\n", x_st1);
     esp = 100;
     while(esp > 1e-9){
         x = x_st2;
         x_st2 = x_st2 - f(x_st2,5)/df(x_st2,5);
         esp = fabs(x_st2 - x);
     }
-    printf("%.9lf\n", x_st2);
+    printf("solution2:%.9lf\n", x_st2);
     esp = 100;
     while(esp > 1e-9){
         x = x_st3;
         x_st3 = x_st3 - f(x_st3,5)/df(x_st3,5);
         esp = fabs(x_st3 - x);
-        printf("esp = %lf\n", esp);
     }
-    printf("%.9lf\n", x_st3);
+    printf("solution3:%.9lf\n", x_st3);
     esp = 100;
     while(esp > 1e-9){
         x = x_st4;
         x_st4 = x_st4 - f(x_st4,5)/df(x_st4,5);
         esp = fabs(x_st4 - x);
     }
-    printf("%.9lf\n", x_st4);
+    printf("solution4:%.9lf\n", x_st4);
     esp = 100;
     while(esp > 1e-9){
         x = x_st5;
         x_st5 = x_st5 - f(x_st5,5)/df(x_st5,5);
         esp = fabs(x_st5 - x);
     }
-    printf("%.9lf\n", x_st5);
+    printf("solution5:%.9lf\n", x_st5);
 
     return 0;
 }
