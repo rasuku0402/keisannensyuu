5ffd7c7f65197b36642fa73abadf0fc19f3eae36
{菊地泰斗} <{taitoo0402@gmail.com}>
2021-11-23 00:54:54 +0900

    first commit

---
+++ matrix_vector/pro2-1.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/*a[i]~a[j]の領域を確保*/
+double *dvector(int i, int j){
+    double *a;
+    if ((a = malloc(((j-i+1)*sizeof(double)))) == NULL){
+        printf("メモリが確保できません(from dvector) \n");
+        exit(1);
+    }
+    return(a-i);
+}
+
+void free_dvector(double *a, int i){
+    free((void*)(a+i));  
+}
+int main(){
+    double *a, *b;
+    int i;
+    int N =5;
+
+    printf("aの領域確保\n"); /*配列aの添え字は1~N*/
+    a = dvector(1,N);
+    printf("aを解放\n");
+
+
+    return 0;
+
+}
